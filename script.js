bc0a06ea9b658e7f46cafa62fb453fcc7964af90
mosot1 <147102984+mosot1@users.noreply.github.com>
2024-02-27 18:08:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+ //toggle icon navbar
+ let navbar = document.querySelector('.navbar');
+
+ navbar.onclick = () => {
+    navbar.classList.toggle('active');
+ }
+
+ // scroll sections
+ let sections = document.querySelectorAll('section');
+ let navLinks = document.querySelectorAll('header nav a');
+
+ window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            // active navbar links
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });           
+        }       
+    });
+    // sticky header
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+    
+    // remove toggle icon and navbar when click navbar links (scroll)
+    navbar.classList.remove('active');
+}    
